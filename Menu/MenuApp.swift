2cb27c9e32a03042218d3e7d817dbf9f3e9bb098
Kyi Zin Latt <kyizinlatt@gmail.com>
2024-03-02 23:14:11 +0700

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Kyi Zin Latt on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
